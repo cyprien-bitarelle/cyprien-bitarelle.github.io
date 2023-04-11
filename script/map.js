d14bfb232022f6153151dd6e391d74c496aca092
Cyprien <bitarelle.cyprien@gmail.com>
2023-04-11 19:44:07 +0200

    test

--- script/map.js
+++ script/map.js
@@ -1,6 +1,6 @@
 //On ajoute la map sur le DOM avec les paramètres voulus.
 let mymap = L.map("mapid").setView([43.67734964920557, 4.631217420101167], 13);
-let tokenLeaflet = vars.LEAFLET;
+let tokenLeaflet = secrets.LEAFLET;
 L.tileLayer(
   "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
   {
